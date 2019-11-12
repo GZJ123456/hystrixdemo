b7a9c2ea94bf312b69fd0feb556027090d8c03f5
GZJ123456 <15511088013@163.com>
2019-11-12 13:55:58 +0800

    Initial commit

---
+++ zull-ff/src/main/java/com/offcn/ZullApplication.java
@@ -0,0 +1,15 @@
+package com.offcn;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@SpringBootApplication
+@SpringCloudApplication
+public class ZullApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZullApplication.class);
+    }
+}
